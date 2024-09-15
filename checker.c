871393b7d17dc758b2da19bf8950be2b280ed2a9
MadharameniSaiTeja <saiteja30930810@gmail.com>
2024-09-15 17:44:45 +0530

    Update checker.c
    Updated the code with new code and test cases

--- checker.c
+++ checker.c
@@ -1,26 +1,41 @@
 #include <stdio.h>
 #include <assert.h>
+#include <stdbool.h>
 
-// int batteryIsOk(float temperature, float soc, float chargeRate) {
-//   if(temperature < 0 || temperature > 45)
-//     printf("Temperature out of range!\n");
-//     return 0;
-//   } else if(soc < 20 || soc > 80) {
-//     printf("State of Charge out of range!\n");
-//     return 0;
-//   } else if(chargeRate > 0.8) {
-//     printf("Charge Rate out of range!\n");
-//     return 0;
-//   }
-//   return 1;
-// }
+bool TemperatureIsOk(float);
+bool SOCIsOk(float);
+bool ChargeRateIsOk(float);
+
 int batteryIsOk(float temperature, float soc, float chargeRate) {
-  if((temperature < 0 || temperature > 45)&&(soc < 20 || soc > 80)&&(chargeRate > 0.8)){
-    return 0;
-  } 
-  return 1;
+
+  return TemperatureIsOk(temperature) && SOCIsOk(soc) && ChargeRateIsOk(chargeRate);
+}
+bool TemperatureIsOk(float temperature){
+    if(temperature < 0 || temperature > 45){
+    return false;//Temperature is NOT OK
+    }
+    return true;//Temperature is OK
+}
+bool SOCIsOk(float soc){
+    if(soc < 20 || soc > 80){
+    return false;//SOC is NOT OK 
+    }
+    return true;//SOC is OK 
+}
+bool ChargeRateIsOk(float chargeRate){
+    if(chargeRate > 0.8){
+    return false;//ChargeRate is NOT OK
+    }
+    return true;//chargerate is OK 
 }
 int main() {
-  assert(batteryIsOk(25, 70, 0.7));
-  assert(!batteryIsOk(50, 85, 0));
+  assert(batteryIsOk(25, 70, 0.7));//battery is OK
+  assert(!batteryIsOk(50, 85, 0.5));//battery is NOT OK
+  assert(!batteryIsOk(70, 85, 0.5));//Temperature is too High
+  assert(!batteryIsOk(-10, 85, 0.5));//Temperature is too Low
+  assert(!batteryIsOk(40, 90, 0.5));//SOC is too High
+  assert(!batteryIsOk(40, 5, 0.5));//SOC is too Low
+  assert(!batteryIsOk(40, 85, 2));//Chargerate is too high
+
+  
 }
